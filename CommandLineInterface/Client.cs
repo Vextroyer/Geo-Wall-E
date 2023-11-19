a207e743f8a44064be13ebf093cdbab868c5e7e8
vextroyer <victormanuel.vena@gmail.com>
2023-11-19 17:27:30 -0500

    Expand CLI

--- CommandLineInterface/Client.cs
+++ CommandLineInterface/Client.cs
@@ -6,9 +6,9 @@ namespace Realend;
 using Frontend;
 
 class Client{
-    public static void Main(){
+    public static void Main(string[] args){
         PrintWelcome();
-        SelectMode();
+        SelectMode(args);
     }
 
     private static void PrintWelcome(){
@@ -21,35 +21,50 @@ class Client{
     /*
     Provides support for different modes of using the application.
     */
-    private static void SelectMode(){
-        RunFromFile();
+    private static void SelectMode(string[] args){
+        Frontend.GSharpCompiler.Flags flags = new GSharpCompiler.Flags();
+        if(args.Contains("--noDebug"))flags.PrintDebugInfo = false;
+        if(args.Contains("--runRepl"))RunREPL(flags);
+        else RunFromFile(flags);//Default behaviour.
     }
-
+    /*
+    This mode establish a REPL session with the user.
+    */
+    private static void RunREPL(Frontend.GSharpCompiler.Flags flags){
+        while(true){
+            Console.Write("> ");
+            string? source = Console.ReadLine();//The code inputed by the user.
+            if(string.IsNullOrEmpty(source))continue;//User just pressed enter or hit to many spaces.
+            Frontend.GSharpCompiler.Response compilerResponse = Frontend.GSharpCompiler.CompileFromSource(source,flags);
+            HandleError(compilerResponse);
+        }
+    }
     /*
     This mode asks user to input a file and then executes the script on the file and shows the output.
     */
-    private static void RunFromFile(){
+    private static void RunFromFile(Frontend.GSharpCompiler.Flags flags){
         Console.WriteLine("Provide files to execute");
         while(true){
             Console.Write("> ");
             string? path = Console.ReadLine();//The path to the file
             if(string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))continue;//User just pressed enter or hit to many spaces.
             
-            //At this point on source its the content of the G# script to be executed.
-            Frontend.GSharpCompiler.Response compilerResponse = Frontend.GSharpCompiler.CompileFromFile(path);
-            if(compilerResponse.HadError){
-                if(compilerResponse.HadErrorReadingFile)ReportError(compilerResponse.Errors.Last().Message);
-                else
+            Frontend.GSharpCompiler.Response compilerResponse = Frontend.GSharpCompiler.CompileFromFile(path,flags);
+            HandleError(compilerResponse);
+        }
+    }
+    private static void HandleError(Frontend.GSharpCompiler.Response compilerResponse){
+        if(compilerResponse.HadError){
+            if(compilerResponse.HadErrorReadingFile)ReportError(compilerResponse.Errors.Last().Message);
+            else
+            {
+                foreach(Frontend.GSharpCompiler.Error error in compilerResponse.Errors)
                 {
-                    foreach(Frontend.GSharpCompiler.Error error in compilerResponse.Errors)
-                    {
-                        ReportError(error.Line,error.Offset,error.Message);
-                    }
+                    ReportError(error.Line,error.Offset,error.Message);
                 }
             }
         }
     }
-
     private static void ReportError(string message){
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(message);
--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 .Phony: dev
+.Phony: repl
+
+repl:
+	dotnet run --project CommandLineInterface --runRepl --noDebug
 
 dev:
 	dotnet run --project CommandLineInterface
